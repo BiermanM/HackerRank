eaf073e61ea026550b84c29d5cb2958f55176489
Matthew Bierman <BiermanM@users.noreply.github.com>
2018-01-03 17:11:42 -0800

    Create Day 3: Intro to Conditional Statements.cpp

---
+++ 30 Days of Code/Day 3: Intro to Conditional Statements.cpp
@@ -0,0 +1,39 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    
+    if (n % 2 == 1)
+        cout << "Weird" << endl;
+    else if (n % 2 == 0 && n >= 2 && n <= 5)
+        cout << "Not Weird" << endl;
+    else if (n % 2 == 0 && n >= 6 && n <= 20)
+        cout << "Weird" << endl;
+    else if (n % 2 == 0 && n > 20)
+        cout << "Not Weird" << endl;
+    
+    return 0;
+}
