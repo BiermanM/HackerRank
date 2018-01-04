a9985c9ebebc2970f3a4d474afaa80afbb399a8b
Matthew Bierman <BiermanM@users.noreply.github.com>
2018-01-03 22:07:43 -0800

    Create Let's Review.java

---
+++ 30 Days of Code/Let's Review.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.nextLine();
+        
+        for (int i = 0; i < n; i++) {
+            String s = scan.nextLine();
+            String even = "", odd = "";
+            
+            for (int j = 0; j < s.length(); j++) {
+                if (j % 2 == 0)
+                    even += s.substring(j, j + 1);
+                else
+                    odd += s.substring(j, j + 1);
+            }
+            System.out.println(even + " " + odd);
+        }
+        
+        scan.close();
+    }
+}
