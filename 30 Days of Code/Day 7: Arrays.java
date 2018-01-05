98e3288f16e8845dd0a8c910b0750ec72f3dfdb6
Matthew Bierman <BiermanM@users.noreply.github.com>
2018-01-04 16:00:19 -0800

    Create Day 7: Arrays.java

---
+++ 30 Days of Code/Day 7: Arrays.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+        in.close();
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[(n - 1) - i] + " ");
+        }
+    }
+}
